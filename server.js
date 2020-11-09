0e246301f387d66826a75e6e05953ce07965287a
S.Otzoy <otzoy97@hotmail.com>
2020-11-08 21:00:46 -0600

    cors añadido

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
+app.use(cors())
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+// rutas
 app.use(require('./server/routes/index'))
 
-app.listen(3000)
+app.listen(3000, ()=> console.log("Servidor listo"))
