f59f4ea0b66912e12c8b681ef2e7aa50ab91843d
S.Otzoy <otzoy97@hotmail.com>
2020-11-09 11:02:58 -0600

    se agregaron componentes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// Components
+import {SignInComponent} from './components/user/sign-in/sign-in.component'
+import {SignUpComponent} from './components/user/sign-up/sign-up.component'
+import {InfoUserComponent} from './components/user/info-user/info-user.component'
+import {VerifyAccountComponent} from './components/user/verify-account/verify-account.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/singin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'singin',
+    component: SignInComponent
+  },
+  {
+    path: 'singup',
+    component: SignUpComponent
+  },
+  {
+    path: 'user/info',
+    component: InfoUserComponent
+  },
+  {
+    path: 'user/auth',
+    component: VerifyAccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/user/sign-in/sign-in.component';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
+import { VerifyAccountComponent } from './components/user/verify-account/verify-account.component';
+import { InfoUserComponent } from './components/user/info-user/info-user.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    VerifyAccountComponent,
+    InfoUserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
