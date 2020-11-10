193928eb51b8684c092f2e220a9f72aa601b3c55
S.Otzoy <otzoy97@hotmail.com>
2020-11-10 10:16:37 -0600

    singin actualizado

--- client/src/app/components/user/sign-in/sign-in.component.ts
+++ client/src/app/components/user/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    email: '',
+    pwd: ''
+  }
+
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  singin() {
+    if (this.user.email.trim.length === 0 || this.user.pwd.length === 0) {
+      alert('Correo o contraseña inválidos')
+      return;
+    }
+    this.userService.singIn(this.user).subscribe((response)=> {
+      if (response.status === 200) {
+        localStorage.setItem('token', response.body.token)
+        localStorage.setItem('esAdmin', response.body.esAdmin)
+        this.router.navigate(['/user/info'])
+      } else if (response.status === 403) {
+        alert('Correo no confirmado')
+      } else if (response.status === 401) {
+        alert('Correo o contraseña incorrectos')
+      } else {
+        this.router.navigate(['/error-500']) 
+      }
+    }, (err)=>{
+      console.error(err)
+    })
   }
 
 }
